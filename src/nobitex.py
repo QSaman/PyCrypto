f732a29f122457240196435ea2e42bd6a235f821
Saman Saadi <QSaman@users.noreply.github.com>
2021-03-14 00:05:23 -0500

    Add nobitex

--- src/nobitex.py
+++ src/nobitex.py
@@ -1,39 +1,30 @@
 #!/bin/python
 
-import socketio
-import requests
-import json
-import time
 import tempfile
-import argparse
 import sys
+import argparse
 
 from urllib.parse import urljoin
 from pathlib import Path, PurePath
 from crypto_common import RatesCache
 from decimal import *
 
-newton_base_endpoint = 'https://api.newton.co'
-newton_rates_endpoint = urljoin(newton_base_endpoint, '/dashboard/api/rates/')
+nobitex_base_endpoint = 'https://api.nobitex.ir'
+nobitex_market_endpoint = urljoin(nobitex_base_endpoint, '/market/stats?srcCurrency=btc,eth,ltc,usdt,xrp,bch,bnb,eos,xlm,etc,trx,doge,pmn&dstCurrency=rls,usdt')
 
-cache_directory = PurePath(tempfile.gettempdir(), 'py_crypto/newton')
-rates_filename = cache_directory / 'rates.json'
+cache_directory = PurePath(tempfile.gettempdir(), 'py_crypto/nobitex')
+market_filename = cache_directory / 'market.json'
 
 cache_expiry_in_seconds = 5
 
-class NewtonService:
-    
-    base_asset_dict = None
-    quote_asset_dict = None
-    ticker_set = None
-    
+class NobitexService:
     def __init__(self):
-        self.rates_cache = RatesCache(newton_rates_endpoint,
+        self.rates_cache = RatesCache(nobitex_market_endpoint,
                                       cache_expiry_in_seconds,
                                       self.load_json_response,
                                       cache_directory,
-                                      rates_filename)
-    
+                                      market_filename)
+
     def find_price(self, base_asset, quote_asset, buy):
         self.rates_cache.get_prices()
         
@@ -43,58 +34,63 @@ class NewtonService:
         price = None
         if ba in self.base_asset_dict and qa in self.base_asset_dict[ba]:
             rate = self.base_asset_dict[ba][qa]
-            # In Newton's perspective (maker) it's ask order, but from customer's perspective (taker) it's a buy order
-            #   Newton (maker) |  Customer (taker)
-            # -----------------|------------------
-            #   ask btc/cad    |  bid btc/cad
-            #   ask btc/cad    |  ask cad/btc
-            # -----------------|------------------
-            #   bid btc/cad    |  ask btc/cad
-            #   bid btc/cad    |  bid cad/btc
-            price = Decimal(rate['ask']) if buy else Decimal(rate['bid'])
+            price = Decimal(rate['bestSell']) if buy else Decimal(rate['bestBuy'])
         elif ba in self.quote_asset_dict and qa in self.quote_asset_dict[ba]:
             rate = self.quote_asset_dict[ba][qa]
-            tp = Decimal(rate['bid']) if buy else Decimal(rate['ask'])
+            tp = Decimal(rate['bestBuy']) if buy else Decimal(rate['bestSell'])
             price = Decimal(1) / tp
         return price
-    
-    def load_json_response(self, response):
+        
+    def load_json_response(self, response): 
         self.base_asset_dict = dict()
         self.quote_asset_dict = dict()
         self.ticker_set = set()
-        for symbol in response["rates"]:
-            rate = dict()
-            #   Newton (maker) |  Customer (taker)
-            # -----------------|------------------
-            #   ask btc/cad    |  bid btc/cad
-            #   ask btc/cad    |  ask cad/btc
-            # -----------------|------------------
-            #   bid btc/cad    |  ask btc/cad
-            #   bid btc/cad    |  bid cad/btc
-            #
-            # So we use Newton perspective:
-            base_asset = symbol["to"].lower() 
-            quote_asset = symbol["from"].lower()
+        
+        def correct_symbol(asset):
+            #The official symbol is IRR not RLS!
+            if asset.lower() == "rls":
+                return "irr"
+            return asset
+        
+        for key, value in response['stats'].items():
+            tickers = key.split('-')
+            if len(tickers) != 2:
+                sys.quit("Invalid symbol ", key)
+            base_asset = correct_symbol(tickers[0].lower())
+            quote_asset = correct_symbol(tickers[1].lower())
             
             self.ticker_set.add(base_asset)
             self.ticker_set.add(quote_asset)
             
-            self.base_asset_dict.setdefault(base_asset, dict()).update({quote_asset : symbol})
-            self.quote_asset_dict.setdefault(quote_asset, dict()).update({base_asset : symbol})
-
+            self.base_asset_dict.setdefault(base_asset, dict()).update({quote_asset : value})
+            self.quote_asset_dict.setdefault(quote_asset, dict()).update({base_asset : value})
+        
 def show_result(amount, base_asset):
     base_asset = base_asset.lower()
     quote_asset = args.quote_asset
+    if base_asset == "irt":
+        base_asset = "irr"
+        amount = amount * 10
     if quote_asset is None:
-        quote_asset = 'btc' if base_asset == 'cad' else 'cad'
-    price = newton.find_price(base_asset, quote_asset, not args.sell)
+        quote_asset = 'btc' if base_asset == 'irr' else 'irt'
+        
+    convert_quote_to_tomans = False
+    if quote_asset == "irt":
+        quote_asset = "irr"
+        convert_quote_to_tomans = True
+    price = nobitex.find_price(base_asset, quote_asset, not args.sell)
+    
+    if convert_quote_to_tomans:
+        price = price / Decimal(10)
+        quote_asset = "irt"
+    
     print(amount * price, quote_asset)
     
 def main():
     parser = argparse.ArgumentParser()
     parser.add_argument("-s", "--sell", action="store_true", default=False, help="The side of order is sell. By default it's --buy")
     parser.add_argument("-b", "--buy", action="store_true", help="The side of order is buy. By default it's --buy")
-    parser.add_argument("-q", "--quote-asset", dest="quote_asset", help="The quote asset. If base-asset is CAD, by default it's BTC; otherwise it's CAD")
+    parser.add_argument("-q", "--quote-asset", dest="quote_asset", help="The quote asset. If base-asset is IRR or IRT, by default it's BTC; otherwise it's IRT")
     parser.add_argument("amount", nargs='?', default=None, type=Decimal, help="The amount of crypto currency")
     parser.add_argument("base_asset", nargs='?', default=None, help="The base asset.")
     
@@ -104,10 +100,10 @@ def main():
     if args.sell and args.buy:
         sys.exit('You can use --sell and --buy together')
         
-    global newton
-    newton = NewtonService()
+    global nobitex
+    nobitex = NobitexService()
     # If amount and base asset is not in argument list, we read them from input. By doing that we can pipe into this script
-    # For example echo "0.0005 btc" | newton.py
+    # For example echo "0.0005 btc" | nobitex.py
     if args.amount is None or args.base_asset is None:
         for line in sys.stdin:
             tokens = line.split()
@@ -116,7 +112,6 @@ def main():
             show_result(amount, base_asset)
     else:
         show_result(args.amount, args.base_asset)
-            
+        
 if __name__ == "__main__":
     main()
-
